781c5b352f8340f408c70ec4593f69bf97420f51
Hans_Tausend <einund.tausend@gmail.com>
2018-10-07 19:03:34 +0200

    not localhost but site

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:3007');
+var socket = io.connect('http://chatapp.digitpay.de/:3007');
 var timeout
 // Query DOM
 var message = document.getElementById('message'),
